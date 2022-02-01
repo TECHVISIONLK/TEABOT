3bcd3790dcb70e4be9c8a41d7ce7be4a4161d59d
TECHVISION LK <96689907+TECHVISIONLK@users.noreply.github.com>
2022-02-01 14:26:47 +0530

    Add files via upload
    TEA BOT - v0.22.1

---
+++ TEABOT/Base.js
@@ -0,0 +1,20 @@
+/* Copyright (C) 2022 TECHVISION LK.
+
+TEA BOT - Saviru Kashmira
+*/
+
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
---
+++ TEABOT/index.js
@@ -0,0 +1,13 @@
+/* Copyright (C) 2022 TECHVISION LK.
+
+TEA BOT - Saviru Kashmira
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
